b6c9b7e78efbd02f43e2c460f6181bafad4dc173
Arnold Paye <arnoldpayet@gmail.com>
2017-08-22 11:44:28 -0400

    initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+# React with bootstrap using reactstrap
+
+## Installation
+```console
+yarn install
+```
+
+## Test
+```console
+yarn test
+```
+
+## Run
+```console
+yarn start
+```
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import { Button } from 'reactstrap';
+
+ReactDOM.render(<Button color="danger">Danger!</Button>, document.getElementById('root'));
